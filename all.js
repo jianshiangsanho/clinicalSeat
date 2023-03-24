5afc8e0cd30e56917413dfc88597fa3ed0659254
jianshiangsanho <125791111+jianshiangsanho@users.noreply.github.com>
2023-03-24 14:47:46 +0800

    Update all.js

--- all.js
+++ all.js
@@ -42,23 +42,6 @@ $(document).ready(function() {
       { "data": "庫存", "defaultContent": "" },
       { "data": "備註", "defaultContent": "" },
     ],
-    "initComplete": function () {
-      this.api().columns(3).every(function () {
-        var column = this;
-        var select = $('<select><option value=""></option></select>')
-          .appendTo($(column.header()).empty())
-          .on('change', function () {
-            var val = $.fn.dataTable.util.escapeRegex(
-              $(this).val()
-            );
-
-            column.search(val ? '^' + val + '$' : '', true, false).draw();
-          });
-
-        column.data().unique().sort().each(function (d, j) {
-          select.append('<option value="' + d + '">' + d + '</option>');
-        });
-      });
-    },
+    
   });
 });
